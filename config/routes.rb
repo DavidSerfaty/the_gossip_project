b7fc36155ecfaa4be027cc515f278774dbf02fa3
DavidSerfaty <david@owanted.io>
2020-10-27 15:30:26 +0100

    Gossip new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
   root 'gossips#index'
+
+  resources :gossips
+  resources :users
+
   get '/contact', to: 'static_pages#contact'
   get '/team', to: 'static_pages#team'
-
-  get '/gossip/:id', to: 'gossips#show', as: 'gossip'
-  get '/user/:id', to: 'users#show', as: 'user'
-
-  get '/welcome/:first_name', to: 'landing_pages#welcome', as: 'welcome'
+  get '/welcome/:first_name', to: 'landing_pages#welcome'
 end
